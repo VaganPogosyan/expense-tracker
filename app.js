80907b010e488bdc705be6c194ee259b1fd5c7a4
Vagan Pogosyan <vagan2502@gmail.com>
2023-10-24 17:54:33 -0400

    finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 
 const express = require("express");
+const cors = require("cors");
 const errorHandler = require("./handlers/errorHandler");
 const mongoose = require("mongoose");
 const userRoutes = require("./modules/users/users.routes");
@@ -9,6 +10,8 @@ const transactionRoutes = require("./modules/transactions/transactions.routes");
 require("dotenv").config();
 
 const app = express();
+// allow our backend api to be accessed from anywhere
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_CONNECTION)
@@ -30,7 +33,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/transactions", transactionRoutes);
 
 // End of all routes:
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "failed",
+    message: "Not Found",
+  });
+});
+
 app.use(errorHandler);
+
 app.listen(8000, () => {
   console.log("Server started successfully");
 });
